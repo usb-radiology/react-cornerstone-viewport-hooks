7bee6b7c8a1e8e4d1ddd7e1bdeeddb5a77ace228
Victor Parmar <vic@smalldata.tech>
2020-06-04 11:40:38 +0200

    B fix exports

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import ReactCornerstoneViewportHooks from "./ReactCornerstoneViewportHooks";
 import ReactCornerstoneViewportHooksHelpers from "./ReactCornerstoneViewportHooksHelpers";
 import RectangleRoiUniqueTool from "./RectangleRoiUniqueTool";
 
-export default {
-  ReactCornerstoneViewportHooks,
-  ReactCornerstoneViewportHooksHelpers,
-  RectangleRoiUniqueTool,
-};
+export { ReactCornerstoneViewportHooksHelpers, RectangleRoiUniqueTool };
+
+export default ReactCornerstoneViewportHooks;
