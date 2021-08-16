23b56cf1b6cde9a546ca68676a9061e2697cd965
Victor Parmar <vic@smalldata.tech>
2021-08-16 15:23:57 +0200

    add RectangleRoiTool example

--- example/src/App.js
+++ example/src/App.js
@@ -3,7 +3,11 @@ import React from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
-import { ReactCornerstoneViewportHooks } from "react-cornerstone-viewport-hooks";
+import {
+  ReactCornerstoneViewportHooks,
+  RectangleRoiUniqueTool,
+  ReactCornerstoneViewportHooksHelpers,
+} from "react-cornerstone-viewport-hooks";
 import "react-cornerstone-viewport-hooks/dist/index.css";
 
 const App = () => {
@@ -38,25 +42,53 @@ const App = () => {
     ],
   };
 
-  return (
-    <div>
-      <h2>Basic Demo</h2>
-      <div style={{ display: "flex", flexWrap: "wrap" }}>
-        <ReactCornerstoneViewportHooks
-          tools={basicDemoState.tools}
-          imageIds={basicDemoState.imageIds}
-          style={{ minWidth: "100%", height: "512px", flex: "1" }}
-        />
-      </div>
+  const rectangleRoiUniqueToolState = {
+    tools: [
+      { name: "StackScrollMouseWheel", mode: "active" },
+      {
+        name: "Zoom",
+        mode: "active",
+        modeOptions: { mouseButtonMask: 2 },
+      },
+      {
+        name: "Pan",
+        mode: "active",
+        modeOptions: { mouseButtonMask: 4 },
+      },
+      {
+        name: ReactCornerstoneViewportHooksHelpers.TOOL_NAMES
+          .RectangleRoiUnique,
+        toolClass: RectangleRoiUniqueTool,
+        mode: "active",
+        modeOptions: { mouseButtonMask: 1 },
+      },
+    ],
+    imageIds: [
+      "dicomweb://s3.amazonaws.com/lury/PTCTStudy/1.3.6.1.4.1.25403.52237031786.3872.20100510032220.11.dcm",
+      "dicomweb://s3.amazonaws.com/lury/PTCTStudy/1.3.6.1.4.1.25403.52237031786.3872.20100510032220.12.dcm",
+    ],
+  };
 
-      <h2>Source / Usage</h2>
-      <div style={{ marginTop: "35px" }}>
-        <SyntaxHighlighter
-          language="jsx"
-          showLineNumbers={true}
-          style={atomDark}
-        >
-          {`const state = {
+  return (
+    <div className="container">
+      <div>
+        <h2>Basic Demo</h2>
+        <div style={{ display: "flex", flexWrap: "wrap" }}>
+          <ReactCornerstoneViewportHooks
+            tools={basicDemoState.tools}
+            imageIds={basicDemoState.imageIds}
+            style={{ minWidth: "100%", height: "512px", flex: "1" }}
+          />
+        </div>
+
+        <h2>Source / Usage</h2>
+        <div style={{ marginTop: "35px" }}>
+          <SyntaxHighlighter
+            language="jsx"
+            showLineNumbers={true}
+            style={atomDark}
+          >
+            {`const state = {
   tools: [
     // Mouse
     {
@@ -92,7 +124,18 @@ const App = () => {
   imageIds={state.imageIds}
   style={{ minWidth: '100%', height: '512px', flex: '1' }}
 />`}
-        </SyntaxHighlighter>
+          </SyntaxHighlighter>
+        </div>
+      </div>
+      <div>
+        <h2>RectangleRoiUniqueTool Demo</h2>
+        <div style={{ display: "flex", flexWrap: "wrap" }}>
+          <ReactCornerstoneViewportHooks
+            tools={rectangleRoiUniqueToolState.tools}
+            imageIds={rectangleRoiUniqueToolState.imageIds}
+            style={{ minWidth: "100%", height: "512px", flex: "1" }}
+          />
+        </div>
       </div>
     </div>
   );
