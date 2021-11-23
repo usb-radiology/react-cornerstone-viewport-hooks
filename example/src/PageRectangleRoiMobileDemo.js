c9e1d5fa39f33ea0794be7825ab5989e8b7f0a2a
Victor Parmar <vic@smalldata.tech>
2021-11-23 15:55:00 +0100

    add warning for mobile roi tool

--- example/src/PageRectangleRoiMobileDemo.js
+++ example/src/PageRectangleRoiMobileDemo.js
@@ -42,10 +42,11 @@ const rectangleRoiMobileToolState = {
   ],
   imageIds: [
     // large image to test zooming
-    "dicomweb://storage.googleapis.com/data.rapmed.net/production/bfeb22d8-083e-425f-a2ba-2624178213b6/8693aafb-fd86-4be5-ad48-bbc92c87ebb5/6b3f3396-7985-46da-959f-b1be8bb5a967.dcm",
+    // "dicomweb://storage.googleapis.com/data.rapmed.net/production/bfeb22d8-083e-425f-a2ba-2624178213b6/8693aafb-fd86-4be5-ad48-bbc92c87ebb5/6b3f3396-7985-46da-959f-b1be8bb5a967.dcm",
     // "dicomweb://storage.googleapis.com/data.rapmed.net/production/bfeb22d8-083e-425f-a2ba-2624178213b6/1695451c-52a2-47f7-9bdf-a34d580c8154/cc8010b9-0ba8-4b23-9182-1e3dc7d96b4a.dcm",
-    // "dicomweb://s3.amazonaws.com/lury/PTCTStudy/1.3.6.1.4.1.25403.52237031786.3872.20100510032220.11.dcm",
-    // "dicomweb://s3.amazonaws.com/lury/PTCTStudy/1.3.6.1.4.1.25403.52237031786.3872.20100510032220.12.dcm",
+    "dicomweb://s3.amazonaws.com/lury/PTCTStudy/1.3.6.1.4.1.25403.52237031786.3872.20100510032220.10.dcm",
+    "dicomweb://s3.amazonaws.com/lury/PTCTStudy/1.3.6.1.4.1.25403.52237031786.3872.20100510032220.11.dcm",
+    "dicomweb://s3.amazonaws.com/lury/PTCTStudy/1.3.6.1.4.1.25403.52237031786.3872.20100510032220.12.dcm",
   ],
 };
 
@@ -63,6 +64,7 @@ const PageRectangleRoiMobileDemo = function() {
       </h5>
 
       <h2>Rectangle Roi Mobile Demo</h2>
+      <h3>(DO NOT USE FOR DESKTOP, does not store toolstate correctly for sectional images)</h3>
       <h4>On mobile pinch zoom is enabled</h4>
       <button
         style={{
